6002afa548548af468144a737dd753bc8281b288
Abhijit Ghorpade <abhijitghorpade121@gmail.com>
2025-06-05 10:06:41 +0530

    Time: 5 ms (91.91%), Space: 42.9 MB (79.76%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -1,35 +1,56 @@
 class Solution {
     public String reverseWords(String s) {
-        s = s.trim();
+        // s = s.trim();
+
+        // StringBuilder sb = new StringBuilder();
+        // Stack<String> stack = new Stack<>();
+    
+        // for(int i = 0; i < s.length(); i++) {
+        //     if(s.charAt(i) != ' ') {
+        //         sb.append(s.charAt(i));
+        //     } else if(s.charAt(i) == ' ' && sb.length() > 0) {
+        //         stack.push(sb.toString());
+        //         sb.setLength(0);
+    
+        //         while(i + 1 < s.length() && s.charAt(i + 1) == ' ') {
+        //             i++;
+        //         }
+        //     }
+        // }
+    
+        // if (sb.length() > 0) {
+        //     stack.push(sb.toString());
+        // }
+    
+        // StringBuilder ans = new StringBuilder();
+        // while(!stack.isEmpty()) {
+        //     ans.append(stack.pop());
+        //     if(!stack.isEmpty()) {
+        //         ans.append(" ");
+        //     }
+        // }
+    
+        // return ans.toString();
+
 
         StringBuilder sb = new StringBuilder();
-        Stack<String> stack = new Stack<>();
-    
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) != ' ') {
-                sb.append(s.charAt(i));
-            } else if(s.charAt(i) == ' ' && sb.length() > 0) {
-                stack.push(sb.toString());
-                sb.setLength(0);
-                // skip multiple spaces
-                while(i + 1 < s.length() && s.charAt(i + 1) == ' ') {
-                    i++;
-                }
+        int j = s.length();
+
+        for(int i = s.length()-2; i >= -1; i--) {
+
+            if(i != -1 && s.charAt(i) != ' ') {
+                continue;
             }
-        }
-    
-        if (sb.length() > 0) {
-            stack.push(sb.toString());
-        }
-    
-        StringBuilder ans = new StringBuilder();
-        while(!stack.isEmpty()) {
-            ans.append(stack.pop());
-            if(!stack.isEmpty()) {
-                ans.append(" ");
+
+            String substr = s.substring(i+1, j);
+
+            if(!substr.isBlank()) {
+                sb.append(substr).append(' ');
             }
+
+           j = i;
         }
-    
-        return ans.toString();
+
+        return sb.toString().trim();
     }
 }
