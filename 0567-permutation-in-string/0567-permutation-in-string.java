dbe8a0bb208954c8a442def9701fe299093e6fd9
Abhijit Ghorpade <abhijitghorpade121@gmail.com>
2025-06-15 22:34:30 +0530

    Time: 7 ms (75.09%), Space: 42.4 MB (98.03%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,30 +1,66 @@
 class Solution {
+    private boolean matches(int[] s1Count, int[] s2Count) {
+        for (int i = 0; i < 26; i++) {
+            if (s1Count[i] != s2Count[i]) return false;
+        }
+        return true;
+    }
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length() > s2.length()) return false;
+        if (s1.length() > s2.length()) return false;
 
-        char a1[] = s1.toCharArray();
-        Arrays.sort(a1);
+        int[] s1Count = new int[26];
+        int[] s2Count = new int[26];
 
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<s1.length(); i++) {
-            sb.append(s2.charAt(i));
+        // Count the frequency of characters in s1 and the first window of s2
+        for (int i = 0; i < s1.length(); i++) {
+            s1Count[s1.charAt(i) - 'a']++;
+            s2Count[s2.charAt(i) - 'a']++;
         }
 
-        for(int i=s1.length(); i<s2.length(); i++) {
-            char a2[] = sb.toString().toCharArray();
-            Arrays.sort(a2);
-
-            if(Arrays.equals(a1, a2)) return true;
-
-            sb.deleteCharAt(0);
-            sb.append(s2.charAt(i));
+        // Slide the window over s2
+        for (int i = 0; i < s2.length() - s1.length(); i++) {
+            if (matches(s1Count, s2Count)) return true;
+            // Update the window
+            s2Count[s2.charAt(i) - 'a']--;
+            s2Count[s2.charAt(i + s1.length()) - 'a']++;
         }
 
-        char a2[] = sb.toString().toCharArray();
-        Arrays.sort(a2);
+        // Check the last window
+        return matches(s1Count, s2Count);
 
-        if(Arrays.equals(a1, a2)) return true;
 
-        return false;
+
+
+
+
+        
+        // first approch
+
+        // if(s1.length() > s2.length()) return false;
+
+        // char a1[] = s1.toCharArray();
+        // Arrays.sort(a1);
+
+        // StringBuilder sb = new StringBuilder();
+        // for(int i=0; i<s1.length(); i++) {
+        //     sb.append(s2.charAt(i));
+        // }
+
+        // for(int i=s1.length(); i<s2.length(); i++) {
+        //     char a2[] = sb.toString().toCharArray();
+        //     Arrays.sort(a2);
+
+        //     if(Arrays.equals(a1, a2)) return true;
+
+        //     sb.deleteCharAt(0);
+        //     sb.append(s2.charAt(i));
+        // }
+
+        // char a2[] = sb.toString().toCharArray();
+        // Arrays.sort(a2);
+
+        // if(Arrays.equals(a1, a2)) return true;
+
+        // return false;
     }
 }
