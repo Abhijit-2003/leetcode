eed1dc3beb0e6e0a0bba7a088eb35c72bee73fe4
Abhijit Ghorpade <abhijitghorpade121@gmail.com>
2025-06-16 11:32:32 +0530

    Time: 0 ms (100%), Space: 40.7 MB (27.97%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,19 +1,32 @@
 class Solution {
-    public int helper(int n, int dp[]) {
-        if(n == 1 || n == 2) return n;
+    // public int helper(int n, int dp[]) {
+    //     if(n == 1 || n == 2) return n;
 
-        if(dp[n] > 0) return dp[n];
+    //     if(dp[n] > 0) return dp[n];
 
-        int sum = helper(n-1, dp) + helper(n-2, dp);
-        dp[n] = sum;
+    //     int sum = helper(n-1, dp) + helper(n-2, dp);
+    //     dp[n] = sum;
 
-        return sum;
-    }
+    //     return sum;
+    // }
 
+    private static List<Integer> list = new ArrayList<>();
     public int climbStairs(int n) {
-        int dp[] = new int[n+1];
-        for(int i=0; i<n; i++) dp[i] = 0;
+        if (n == 0 || n == 1) {
+            return 1;
+        }
 
-        return helper(n, dp);
+        int[] dp = new int[n+1];
+        dp[0] = dp[1] = 1;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+
+        // int dp[] = new int[n+1];
+        // for(int i=0; i<n; i++) dp[i] = 0;
+
+        // return helper(n, dp);
     }
 }
