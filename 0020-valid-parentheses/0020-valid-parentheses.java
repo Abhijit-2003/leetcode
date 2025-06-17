5e15554b933931e09b74df65a6b2dafbfc39cda0
Abhijit Ghorpade <abhijitghorpade121@gmail.com>
2025-06-17 09:16:37 +0530

    Time: 4 ms (12.02%), Space: 42.2 MB (11.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,29 +1,24 @@
 class Solution {
     public boolean isValid(String s) {
-        // Create hashmap to store the pairs...
-        HashMap<Character, Character> Hmap = new HashMap<Character, Character>();
-        Hmap.put(')','(');
-        Hmap.put('}','{');
-        Hmap.put(']','[');
-        // Create stack data structure...
-        Stack<Character> stack = new Stack<Character>();
-        // Traverse each charater in input string...
-        for (int idx = 0; idx < s.length(); idx++){
-            // If open parentheses are present, push it to stack...
-            if (s.charAt(idx) == '(' || s.charAt(idx) == '{' || s.charAt(idx) == '[') {
-                stack.push(s.charAt(idx));
-                continue;
-            }
-            // If the character is closing parentheses, check that the same type opening parentheses is being pushed to the stack or not...
-            // If not, we need to return false...
-            if (stack.size() == 0 || Hmap.get(s.charAt(idx)) != stack.pop()) {
-                return false;
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> map = new HashMap<>();
+
+        map.put(')', '(');
+        map.put(']', '[');
+        map.put('}', '{');
+
+        for(int i=0; i<s.length(); i++) {
+            if(!stack.isEmpty() && map.containsKey(s.charAt(i))) {
+                if(stack.peek() != map.get(s.charAt(i))) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            } else {
+                stack.push(s.charAt(i));
             }
         }
-        // If the stack is empty, return true...
-        if (stack.size() == 0) {
-            return true;
-        }
-        return false;
+
+        return stack.isEmpty();
     }
 }
