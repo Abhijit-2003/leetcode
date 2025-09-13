9894f9b716a5030e4445877aba399eb4ff392c53
Abhijit Ghorpade <abhijitghorpade121@gmail.com>
2025-09-13 16:42:17 +0530

    Time: 19 ms (20.66%), Space: 45.7 MB (55.9%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -12,22 +12,36 @@ class Solution {
 
         StringBuilder ans = new StringBuilder();
 
-        while(!map.isEmpty()) {
-            char maxChar = 0;
-            int maxValue = 0;
+        // while(!map.isEmpty()) {
+        //     char maxChar = 0;
+        //     int maxValue = 0;
 
-            for(char c : map.keySet()) {
-                if(map.get(c) > maxValue) {
-                    maxChar = c;
-                    maxValue = map.get(c);
-                }
+        //     for(char c : map.keySet()) {
+        //         if(map.get(c) > maxValue) {
+        //             maxChar = c;
+        //             maxValue = map.get(c);
+        //         }
+        //     }
+
+        //     for(int i=0; i<maxValue; i++) {
+        //         ans.append(maxChar);
+        //     }
+
+        //     map.remove(maxChar);
+        // }
+
+        PriorityQueue<Map.Entry<Character, Integer>> maxHeap = new PriorityQueue<>(
+            (a, b) -> b.getValue() - a.getValue()
+        );
+
+        maxHeap.addAll(map.entrySet());
+
+        while(!maxHeap.isEmpty()) {
+            Map.Entry<Character, Integer> entry = maxHeap.poll();
+
+            for(int i=0; i<entry.getValue(); i++) {
+                ans.append(entry.getKey());
             }
-
-            for(int i=0; i<maxValue; i++) {
-                ans.append(maxChar);
-            }
-
-            map.remove(maxChar);
         }
 
         return ans.toString();
