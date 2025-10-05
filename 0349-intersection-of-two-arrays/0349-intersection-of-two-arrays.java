d7d50776f76013a86137dbd5983006236da41071
Abhijit Ghorpade <abhijitghorpade121@gmail.com>
2025-10-05 16:04:11 +0530

    Time: 0 ms (100%), Space: 43.1 MB (83.63%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,22 +1,38 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        Set<Integer> set = new HashSet<>();
+        // Set<Integer> set = new HashSet<>();
 
-        for(int num : nums1) {
-            set.add(num);
-        }
+        // for(int num : nums1) {
+        //     set.add(num);
+        // }
 
-        int[] ans = new int[nums1.length];
-        int count = 0;
+        // int[] ans = new int[nums1.length];
+        // int count = 0;
 
-        for(int num : nums2) {
-            if(set.contains(num)) {
-                set.remove(num);
-                ans[count] = num;
-                count++;
+        // for(int num : nums2) {
+        //     if(set.contains(num)) {
+        //         set.remove(num);
+        //         ans[count] = num;
+        //         count++;
+        //     }
+        // }
+
+        // return Arrays.copyOfRange(ans, 0, count);
+
+
+        boolean[] seen = new boolean[1001];
+        for (int num : nums1) 
+            seen[num] = true;
+
+        int[] res = new int[nums1.length];
+        int k=0;
+
+        for (int num : nums2)
+            if (seen[num] == true) {
+                seen[num] = false;
+                res[k++] = num;
             }
-        }
 
-        return Arrays.copyOfRange(ans, 0, count);
+        return Arrays.copyOfRange(res,0,k);
     }
 }
