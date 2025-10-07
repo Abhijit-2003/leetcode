8b4c65794396653fea04eb46540bc572159a0886
Abhijit Ghorpade <abhijitghorpade121@gmail.com>
2025-10-07 11:02:56 +0530

    Time: 5 ms (94.78%), Space: 45.4 MB (48.89%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,20 +1,33 @@
 class Solution {
     public int firstUniqChar(String s) {
-        Map<Character, Integer> map = new LinkedHashMap<>();
+        // Map<Character, Integer> map = new LinkedHashMap<>();
+
+        // for(char ch : s.toCharArray()) {
+        //     if(map.containsKey(ch)) {
+        //         map.put(ch, map.get(ch) + 1);
+        //     } else {
+        //         map.put(ch, 1);
+        //     }
+        // }
+
+        // Set<Character> keys = map.keySet();
+        // for (char key : keys) {
+        //     if(map.get(key) == 1) {
+        //         return s.indexOf(key);
+        //     }
+        // }
+
+        // return -1;
+
+
+        int freq[] = new int[26];
 
         for(char ch : s.toCharArray()) {
-            if(map.containsKey(ch)) {
-                map.put(ch, map.get(ch) + 1);
-            } else {
-                map.put(ch, 1);
-            }
+            freq[ch - 'a']++;
         }
 
-        Set<Character> keys = map.keySet();
-        for (char key : keys) {
-            if(map.get(key) == 1) {
-                return s.indexOf(key);
-            }
+        for(char ch : s.toCharArray()) {
+            if(freq[ch - 'a'] == 1) return s.indexOf(ch);
         }
 
         return -1;
