7938f1178ca7e58347890c79c7598487c8aa4e3a
Abhijit Ghorpade <abhijitghorpade121@gmail.com>
2026-03-03 21:48:33 +0530

    Time: 4 ms (40.02%), Space: 20.3 MB (60.44%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,10 +1,27 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Brute Force Approch
+
+        # n = len(nums)
+
+        # for i in range(n) :
+        #     for j in range(i+1, n) :
+        #         if (nums[i] + nums[j]) == target :
+        #             return list([i, j])
+        
+        # return list()
+
+        # Optimal Solution 
+
         n = len(nums)
+        d = {}
 
         for i in range(n) :
-            for j in range(i+1, n) :
-                if (nums[i] + nums[j] == target) :
-                    return list([i, j])
-        
+            need = target - nums[i]
+
+            if need in d :
+                return list([d.get(need), i])
+            else :
+                d[nums[i]] = i
+
         return list()
