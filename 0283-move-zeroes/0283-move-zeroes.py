aa12729b7d39f8467ce695b7eabd098a9ef6a392
Abhijit Ghorpade <abhijitghorpade121@gmail.com>
2026-03-10 23:09:04 +0530

    Time: 3 ms (83.07%), Space: 20.4 MB (97.17%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,11 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        j = 0
-        for i in range(len(nums)) :
-            if nums[i] != 0 :
-                nums[i],nums[j] = nums[j],nums[i]
-                j+= 1
+        # j = 0
+        # for i in range(len(nums)) :
+        #     if nums[i] != 0 :
+        #         nums[i],nums[j] = nums[j],nums[i]
+        #         j+= 1
+
+        nums.sort(key=lambda x: x==0)
 
 
 
